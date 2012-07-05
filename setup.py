356f37d7bc7bbb0d86f3f13ea31f0024f7db6dfd
Nick Wilson <nick@njwilson.net>
2012-07-05 10:41:07 -0700

    Fix typo in project URL

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ distutils.core.setup(
     version=version,
     author="The BigML Team",
     author_email="bigml@bigml.com",
-    url="https:/bigml.com/developers",
+    url="https://bigml.com/developers",
     download_url="https://github.com/bigmlcom/python",
     license="http://www.apache.org/licenses/LICENSE-2.0",
     setup_requires = [],

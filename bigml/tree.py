cb4aa54795f870105ccc3a9c9d8b4c12dcff62c0
Francisco J. Martin aka aficionado <martin@bigml.com>
2012-08-13 13:18:42 -0700

    Small refactor and add rule generator

--- bigml/tree.py
+++ bigml/tree.py
@@ -19,7 +19,20 @@
 
 This is a simple implementation of a Tree-like Predictive Model
 
+from bigml.api import BigML
+from bigml.tree import Tree
+
+api = BigML()
+
+model = api.get_model('model/5026965515526876630001b2')
+
+tree = Tree(model['object']['model']['root'], model['object']['model']['fields'], model['object']['objective_fields'])
+tree.predict({"000002": 2.46, "000003": 1})
+tree.rules()
+
 """
+import logging
+LOGGER = logging.getLogger('BigML')
 
 import operator
 
@@ -38,61 +51,68 @@ class Predicate(object):
         self.field = field
         self.value = value
 
-class Node(object):
+class Tree(object):
 
-    def __init__(self, node):
-        self.output = node['output']
-        if node['predicate'] == True:
+    def __init__(self, tree, fields, objective_field=None):
+
+        self.fields = fields
+        if objective_field and isinstance(objective_field, list):
+            self.objective_field = objective_field[0]
+        else:
+            self.objective_field = objective_field
+
+        self.output = tree['output']
+        if tree['predicate'] == True:
             self.predicate = True
         else:
             self.predicate = Predicate(
-                node['predicate']['operator'],
-                node['predicate']['field'],
-                node['predicate']['value'])
+                tree['predicate']['operator'],
+                tree['predicate']['field'],
+                tree['predicate']['value'])
         children = []
-        if 'children' in node:
-            for child in node['children']:
-                children.append(Node(child))
+        if 'children' in tree:
+            for child in tree['children']:
+                children.append(Tree(child, fields, objective_field))
         self.children = children
-        self.count = node['count']
-        self.distribution = node['distribution']
+        self.count = tree['count']
+        self.distribution = tree['distribution']
 
-
-class Prediction(object):
-    def split(self, node):
-        field = set([child.predicate.field for child in node.children])
+    def split(self, children):
+        field = set([child.predicate.field for child in children])
         if len(field) == 1:
             return field.pop()
 
-    def __init__(self, node, input):
-        self.node = node
-        self.input = input
-
-    def predict(self):
-        if self.node.children and self.split(self.node) in self.input:
-            for child in self.node.children:
-                if apply(OPERATOR[child.predicate.operator], [self.input[child.predicate.field], child.predicate.value]):
-                    return Prediction(child, self.input).predict()
+    def predict(self, input):
+        if self.children and self.split(self.children) in input:
+            for child in self.children:
+                if apply(OPERATOR[child.predicate.operator], [input[child.predicate.field], child.predicate.value]):
+                    print("%s %s %s\n" % (self.fields[child.predicate.field]['name'], child.predicate.operator, child.predicate.value))
+                    return child.predict(input)
                     break;
         else:
-            return self.node.output
+            return self.output
+
+    def rules(self, depth=0):
+        if self.children:
+            for child in self.children:
+                print("%s IF %s %s %s %s" %
+                    ('   ' * depth,
+                    self.fields[child.predicate.field]['name'],
+                    child.predicate.operator,
+                    child.predicate.value,
+                    "AND" if child.children else "THEN"))
+                child.rules(depth+1)
+        else:
+            if self.predicate == True:
+                print("%s %s = %s" % (
+                    '   ' * depth,
+                    self.fields[self.objective_field]['name'] if self.objective_field else "Prediction",
+                    self.output))
+            else:
+                print("%s %s = %s" % (
+                    '   ' * depth,
+                    self.fields[self.objective_field]['name'] if self.objective_field else "Prediction",
+                    self.output))
 
 
-class Rule(object):
 
-    def __init__(self, _if, _then):
-        self._if = _if
-        self._then = _then
-
-    def pprint(self):
-        print "IF %s THEN %s" % (self._if, self._then)
-
-
-# from bigml.api import BigML
-# api = BigML()
-# from bigml.tree import Node
-# from bigml.tree import Prediction
-
-# model = api.get_model('model/5026965515526876630001b2')
-
-# t = Node(model['object']['model']['root'])

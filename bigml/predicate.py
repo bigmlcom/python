bd2b9a56e6d0f695b1ce3ed67e0875692b74f92a
Mercè Martín <merce@bigml.com>
2013-09-25 01:32:53 +0200

    Adapting Predicate to the token_mode options for text analysis

--- bigml/predicate.py
+++ bigml/predicate.py
@@ -37,16 +37,55 @@ OPERATOR = {
     ">=": operator.ge,
     ">": operator.gt
 }
-
+TM_TOKENS = 'tokens_only'
+TM_FULL_TERM = 'full_terms_only'
+TM_ALL = 'all'
 
 def term_matches(text, forms_list, options):
     """ Counts the number of occurences of the words in forms_list in the text
 
+        You the terms in forms_list can either be tokens or full terms. The
+        matching for tokens is contains and for full terms is equals.
     """
+    token_mode = options.get('token_mode', TM_TOKENS)
+    case_sensitive = options.get('case_sensitive', False)
+    first_term = forms_list[0]
+    if token_mode == TM_FULL_TERM:
+        return full_term_match(text, first_term, case_sensitive)
+    else:
+        # In token_mode='all' we will match full terms using equals and 
+        # tokens using contains
+        if token_mode == TM_ALL and len(forms_list) == 1:
+            pattern = re.compile(r'^.+\b.+$', re.U)
+            if re.match(pattern, first_term):
+                return full_term_match(text, first_term, case_sensitive)
+        return term_matches_tokens(text, forms_list, case_sensitive)
 
+def full_term_match(text, full_term, case_sensitive):
+    """Counts the match for full terms according to the case_sensitive option
+
+    """
+    if not case_sensitive:
+        text = text.lower()
+        full_term = full_term.lower()
+    return int(text == full_term)
+
+def get_tokens_flags(case_sensitive):
+    """Sets flags for regular expression matching depending on text analysis
+       options
+
+    """
     flags = re.U
-    if not options.get('case_sensitive', False):
+    if not case_sensitive:
         flags = (re.I | flags)
+    return flags
+
+
+def term_matches_tokens(text, forms_list, case_sensitive):
+    """ Counts the number of occurences of the words in forms_list in the text
+
+    """   
+    flags = get_tokens_flags(case_sensitive)
     expression = ur'(\b|_)%s(\b|_)' % '(\\b|_)|(\\b|_)'.join(forms_list)
     pattern = re.compile(expression, flags=flags)
     matches = re.findall(pattern, text)
@@ -63,34 +102,51 @@ class Predicate(object):
         self.value = value
         self.term = term
 
+    def is_full_term(self, fields):
+        """Returns a boolean showing if a term is considered as a full_term
+
+        """
+        if self.term is not None:
+            options = fields[self.field]['term_analysis']
+            token_mode = options.get('token_mode', TM_TOKENS)
+            if token_mode == TM_FULL_TERM:
+                return True
+            if token_mode == TM_ALL:
+                pattern = re.compile(r'^.+\b.+$', re.U)
+                return re.match(pattern, self.term)
+        return False
+
     def to_rule(self, fields, label='name'):
         """ Builds rule string from a predicate
 
         """
         name = fields[self.field][label]
+        full_term = self.is_full_term(fields)
         if self.term is not None:
             relation_suffix = ''
             if ((self.operator == '<' and self.value <= 1) or
                     (self.operator == '<=' and self.value == 0)):
-                relation_literal = 'does not contain'
+                relation_literal = ('is not equal to' if full_term
+                                    else 'does not contain')
             else:
-                relation_literal = 'contains'
-                if self.operator == '<=':
-                    relation_suffix = (
-                        'no more than %s %s' %
-                        (self.value, plural('time', self.value)))
-                elif self.operator == '>=':
-                    relation_suffix = (
-                        '%s %s at most' %
-                        (self.value, plural('time', self.value)))
-                elif self.operator == '>' and self.value != 0:
-                    relation_suffix = (
-                        'more than %s %s' %
-                        (self.value, plural('time', self.value)))
-                elif self.operator == '<':
-                    relation_suffix = (
-                        'less than %s %s' %
-                        (self.value, plural('time', self.value)))
+                relation_literal = 'is equal to' if full_term else 'contains'
+                if not full_term:
+                    if self.operator == '<=':
+                        relation_suffix = (
+                            'no more than %s %s' %
+                            (self.value, plural('time', self.value)))
+                    elif self.operator == '>=':
+                        relation_suffix = (
+                            '%s %s at most' %
+                            (self.value, plural('time', self.value)))
+                    elif self.operator == '>' and self.value != 0:
+                        relation_suffix = (
+                            'more than %s %s' %
+                            (self.value, plural('time', self.value)))
+                    elif self.operator == '<':
+                        relation_suffix = (
+                            'less than %s %s' %
+                            (self.value, plural('time', self.value)))
             return u"%s %s %s %s" % (name, relation_literal,
                                      self.term, relation_suffix)
         return u"%s %s %s" % (name,

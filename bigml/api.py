3e21aa6aced32c6be1818ae64136e779ca1d0e50
Mercè Martín <merce@bigml.com>
2013-11-25 23:50:35 +0100

    Adding extra info to api error messages

--- bigml/api.py
+++ bigml/api.py
@@ -347,6 +347,9 @@ def error_message(resource, resource_type='resource', method=None):
         if ('status' in error_info and
                 'message' in error_info['status']):
             error = error_info['status']['message']
+            extra = error_info['status'].get('extra', None)
+            if extra is not None:
+                error += ": %s" % extra
         if code == HTTP_NOT_FOUND and method == 'get':
             alternate_message = ''
             if BIGML_DOMAIN != 'bigml.io':

bf045fc52b39c56a3b31fb64c8edd86134dcf073
Mercè Martín Prats <merce@bigml.com>
2017-07-01 13:32:40 +0200

    Adding time-series as a new model to be evaluated

--- bigml/evaluationhandler.py
+++ bigml/evaluationhandler.py
@@ -31,7 +31,7 @@ from bigml.resourcehandler import ResourceHandler
 from bigml.resourcehandler import (check_resource_type,
                                    get_evaluation_id)
 from bigml.constants import (EVALUATION_PATH, MODEL_PATH, ENSEMBLE_PATH,
-                             LOGISTIC_REGRESSION_PATH)
+                             LOGISTIC_REGRESSION_PATH, TIME_SERIES_PATH)
 
 
 class EvaluationHandler(ResourceHandler):
@@ -58,7 +58,8 @@ class EvaluationHandler(ResourceHandler):
         if args is not None:
             create_args.update(args)
 
-        model_types = [ENSEMBLE_PATH, MODEL_PATH, LOGISTIC_REGRESSION_PATH]
+        model_types = [ENSEMBLE_PATH, MODEL_PATH, LOGISTIC_REGRESSION_PATH,
+                       TIME_SERIES_PATH]
         origin_resources_checked = self.check_origins(
             dataset, model, create_args, model_types=model_types,
             wait_time=wait_time, retries=retries)

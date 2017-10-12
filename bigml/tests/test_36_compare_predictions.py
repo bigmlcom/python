a05a37f94c2b6b2fd1f93447934b86128666239f
Mercè Martín Prats <merce@bigml.com>
2017-10-12 23:39:27 +0200

    Adding tests for deepnet predictions

---
+++ bigml/tests/test_36_compare_predictions.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+#
+# Copyright 2015-2017 BigML
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+""" Comparing remote and local predictions
+
+"""
+from world import world, setup_module, teardown_module, show_doc
+import create_source_steps as source_create
+import create_dataset_steps as dataset_create
+import create_model_steps as model_create
+import create_prediction_steps as prediction_create
+import compare_predictions_steps as prediction_compare
+
+
+class TestComparePrediction(object):
+
+    def setup(self):
+        """
+            Debug information
+        """
+        print "\n-------------------\nTests in: %s\n" % __name__
+
+    def teardown(self):
+        """
+            Debug information
+        """
+        print "\nEnd of tests in: %s\n-------------------\n" % __name__
+
+    def test_scenario1(self):
+        """
+            Scenario: Successfully comparing predictions for deepnets:
+                Given I create a data source uploading a "<data>" file
+                And I wait until the source is ready less than <time_1> secs
+                And I create a dataset
+                And I wait until the dataset is ready less than <time_2> secs
+                And I create a deepnet with objective "<objective>" and "<params>"
+                And I wait until the deepnet is ready less than <time_3> secs
+                And I create a local deepnet
+                When I create a prediction for "<data_input>"
+                Then the prediction for "<objective>" is "<prediction>"
+                And I create a local prediction for "<data_input>"
+                Then the local prediction is "<prediction>"
+
+                Examples:
+                | data             | time_1  | time_2 | time_3 | data_input                             | objective | prediction  | params,
+
+
+        """
+        examples = [
+            ['data/iris.csv', '10', '50', '30000', '{"petal width": 4}', '000004', 'Iris-virginica', '{}'],
+            ['data/iris.csv', '10', '50', '30000', '{"sepal length": 4.1, "sepal width": 2.4}', '000004', 'Iris-setosa', '{}'],
+            ['data/iris_missing2.csv', '10', '50', '30000', '{}', '000004', 'Iris-setosa', '{}'],
+            ['data/spam.csv', '10', '50', '30000', '{}', '000000', 'ham', '{}']]
+        show_doc(self.test_scenario1, examples)
+
+        for example in examples:
+            print "\nTesting with:\n", example
+            source_create.i_upload_a_file(self, example[0])
+            source_create.the_source_is_finished(self, example[1])
+            dataset_create.i_create_a_dataset(self)
+            dataset_create.the_dataset_is_finished_in_less_than(self, example[2])
+            model_create.i_create_a_deepnet_with_objective_and_params(self, example[5], example[7])
+            model_create.the_deepnet_is_finished_in_less_than(self, example[3])
+            prediction_compare.i_create_a_local_deepnet(self)
+            prediction_create.i_create_a_deepnet_prediction(self, example[4])
+            prediction_create.the_prediction_is(self, example[5], example[6])
+            prediction_compare.i_create_a_local_deepnet_prediction(self, example[4])
+            prediction_compare.the_local_prediction_is(self, example[6])

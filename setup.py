70131ea2b4870077ce433a202ba9b5ab357dc244
Mercè Martín Prats <merce@bigml.com>
2019-11-25 21:37:22 +0100

    Setting a minimal version for bigml-chronos

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ PYTHON_VERSION = sys.version_info[0:3]
 PYTHON_REQUESTS_CHANGE = (2, 7, 9)
 REQUESTS_VERSION = "requests==2.5.3" if \
     PYTHON_VERSION < PYTHON_REQUESTS_CHANGE else "requests"
-INSTALL_REQUIRES = ["unidecode", "bigml-chronos"]
+INSTALL_REQUIRES = ["unidecode", "bigml-chronos>0.3.7"]
 if PYTHON_VERSION[0] < 3:
     INSTALL_REQUIRES.append('poster')
 else:

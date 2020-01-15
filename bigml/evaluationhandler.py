50d00f7dfb14c91a103bb948e9c64533bc5767b8
Mercè Martín Prats <merce@bigml.com>
2020-01-15 18:54:59 +0100

    Copyright update

--- bigml/evaluationhandler.py
+++ bigml/evaluationhandler.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 #
-# Copyright 2014-2019 BigML
+# Copyright 2014-2020 BigML
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain

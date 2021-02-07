6b153ed9df63dcc8e8de44d7f6401ac84cc68d3a
Mercè Martín Prats <merce@bigml.com>
2021-02-07 16:34:08 +0100

    Updating copyright notice

--- bigml/tests/test_38_project_connection.py
+++ bigml/tests/test_38_project_connection.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Copyright 2018-2020 BigML
+# Copyright 2018-2021 BigML
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain

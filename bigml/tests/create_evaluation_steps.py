04521ee917193fedf23c4f472fb6960ea8ada683
Mercè Martín Prats <merce@bigml.com>
2022-03-04 15:17:26 +0100

    New version and copyright update

--- bigml/tests/create_evaluation_steps.py
+++ bigml/tests/create_evaluation_steps.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2012, 2015-2021 BigML
+# Copyright 2012, 2015-2022 BigML
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
